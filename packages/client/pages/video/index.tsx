b9db234d43a480e5c06413fca6189830f5104181
kevinroan <kevinnroan@gmail.com>
2021-03-30 10:55:35 -0400

    make video page to be empty

--- packages/client/pages/video/index.tsx
+++ packages/client/pages/video/index.tsx
@@ -1,11 +1,16 @@
-import Layout from '@xr3ngine/client-core/components/ui/Layout/HarmonyLayout';
+import { EmptyLayout } from '@xr3ngine/client-core/components/ui/Layout/EmptyLayout';
+import Scene from '@xr3ngine/client-core/components/scenes/location';
 import { useRouter } from 'next/router';
 import React from 'react';
 import NoSSR from 'react-no-ssr';
 
 const VideoPage = () => {
   return (
-    <Layout pageTitle="Video"/>
+    <EmptyLayout>
+       {/* <Scene/> */}
+        <div>
+        </div>
+    </EmptyLayout>
   );
 };
 
